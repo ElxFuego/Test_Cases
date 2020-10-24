926a5c8a1c53d362810a9317c2e59ce3ab2dccc4
ElxFuego <71596277+ElxFuego@users.noreply.github.com>
2020-10-24 13:36:29 +0200

    Add files via upload

---
+++ Automated_TC/"AutomationPractice" website/src/main/java/Utils.java
@@ -0,0 +1,6 @@
+// Configuration class for Form Automation project
+
+public class Utils {
+    final static String BASE_URL1 = "http://automationpractice.com/index.php";
+    final static String CHROME_DRIVER_LOCATION = "C:\\Program Files\\JAVA_Projects";
+}
